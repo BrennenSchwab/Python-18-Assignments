2380393ccb1167b5791c0226807472d105d8c009
BrennenSchwab <86125051+BrennenSchwab@users.noreply.github.com>
2021-07-28 01:27:09 -0700

    Add files via upload
    All main examples and further study 1 complete. to add other fs later

---
+++ python-ds-practice/07_multiple_letter_count/multiple_letter_count.py
@@ -0,0 +1,15 @@
+def multiple_letter_count(phrase):
+    """Return dict of {ltr: frequency} from phrase.
+
+        >>> multiple_letter_count('yay')
+        {'y': 2, 'a': 1}
+
+        >>> multiple_letter_count('Yay')
+        {'Y': 1, 'a': 1, 'y': 1}
+    """
+    thing={}
+
+    for letter in phrase:
+        thing[letter] = thing.get(letter, 0)+1
+    
+    return thing
